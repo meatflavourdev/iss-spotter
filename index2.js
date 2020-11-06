20203abdb1ead5c044e7e7434098f839c861f05e
Jeremy Felix D <me@jeremyfelix.com>
2020-11-06 00:56:12 -0800

    nextISSTimesForMyLocation promise function

--- index2.js
+++ index2.js
@@ -1,18 +1,8 @@
 // index.js
-const humanizeDuration = require("humanize-duration");
-const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require("./iss_promised");
+const { nextISSTimesForMyLocation } = require("./iss_promised");
+const { printPassTimes } = require("./printPassTimes");
 
-const printPassTimes = function(passTimes) {
-  const data = JSON.parse(passTimes);
-  for (const flyover of data.response) {
-    const timeString = new Date(flyover.risetime * 1000).toString();
-    console.log(`Next pass at ${timeString} for ${humanizeDuration(flyover.duration * 1000)}!`);
-  }
-};
-
-fetchMyIP()
-  .then(fetchCoordsByIP)
-  .then(fetchISSFlyOverTimes)
+nextISSTimesForMyLocation()
   .then((passTimes) => {
     printPassTimes(passTimes);
   })
