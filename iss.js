891e9dbb080a2f6ad06c2f374e574f24011c0fb5
Jeremy Felix D <me@jeremyfelix.com>
2020-11-05 21:07:44 -0800

    Additional error checking

--- iss.js
+++ iss.js
@@ -11,14 +11,21 @@ const URL = "https://api.ipify.org/?format=json";
  *   - An error, if any (nullable)
  *   - The IP address as a string (null if error). Example: "162.245.144.188"
  */
-const fetchMyIP = function(callback) { 
+const fetchMyIP = function(callback) {
   request(URL, (error, response, body) => {
+    // error can be set if invalid domain, user is offline, etc.
     if (error) {
-      callback(error);
+      callback(error, null);
+      return;
+    }
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
       return;
     }
     const data = JSON.parse(body);
-    callback(undefined, data.ip);
+    callback(null, data.ip);
   });
 };
 
